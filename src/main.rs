558d9ee0194764b47f5050e899b8ddaedbcb799e
Eugene Ryzhikov <Eugene.Ryzhikov@fpl.com>
2023-09-11 17:29:53 -0400

    Adjust icon hover text position for better visibility
    Update the position of the icon hover text to the middle, in order to increase visibility. The previous location at the top had usability issues as the text was sometimes not visible to the users. This CSS change helps to improve user experience, especially for new users who are unfamiliar with the icon designations.

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,12 @@ fn App(cx: Scope) -> impl IntoView {
     view! { cx,
         <Stylesheet id="leptos" href="/pkg/tailwind.css"/>
         <div class="m-5 flex flex-row  flex-wrap gap-2">
-
             {
                 ALL_ICONS.iter().map(|it|
                     view! { cx,
                         <div class="p-3.5 bg-gray-100 rounded-lg hover:bg-gray-200 border border-gray-100 hover:border-gray-400/50 hover:border-1 group">
                             <Icon kind={*it}/>
-                            <div class="absolute group:top-0.95 group:left-1/2 -translate-x-1/2 -translate-y-5/6 z-1 opacity-0 transition-opacity group-hover:opacity-100 p-1 px-2 text-xs text-white bg-orange-700 rounded" >
+                            <div class="absolute group:top-1/2 group:left-1/2 -translate-x-1/2 translate-y-5 z-1 opacity-0 transition-opacity group-hover:opacity-100 p-1 px-2 text-xs text-white bg-orange-700/90 rounded" >
                                {it.name}
                             </div>
                         </div>
