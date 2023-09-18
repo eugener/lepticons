d8e0837532dc15aa6bfd1a9153eaf30857329826
Eugene Ryzhikov <eryzhikov@gmail.com>
2023-09-17 22:52:14 -0400

    Change Icon default parameters and add class parameter
    Modified Lucide Icon component in `lib.rs` to include a 'class' parameter, and changed the data types for 'fill' and 'stroke' parameters to &'static str. The 'class' parameter allows users to add custom classes to an Icon element providing more customization options. The 'fill' and 'stroke' parameters type change has been made to ensure consistency in parameter data types.

--- lucide_icons/src/lib.rs
+++ lucide_icons/src/lib.rs
@@ -10,21 +10,39 @@ use leptos::*;
 use strum::EnumProperty;
 
 const DEFAULT_SIZE: u16 = 24;
-const DEFAULT_FILL: &str = "none";
-const DEFAULT_STROKE: &str = "black";
+const DEFAULT_FILL: &'static str = "none";
+const DEFAULT_STROKE: &'static str = "black";
 const DEFAULT_STROKE_WIDTH: u16 = 2;
+
+// TODO
+//      add support for all the other svg attributes
+//      add support for crate features: default | feature1, feature2, ...
+
 
 #[component]
 pub fn Icon(
     icon: LucideIcon,
-    #[prop(default = DEFAULT_SIZE)] size: u16,
-    #[prop(default = DEFAULT_FILL.to_string())] fill: String,
-    #[prop(default = DEFAULT_STROKE.to_string())] stroke: String,
-    #[prop(default = DEFAULT_STROKE_WIDTH)] stroke_width: u16,
+
+    #[prop(default = "")]
+    class: &'static str,
+
+    #[prop(default = DEFAULT_SIZE)]
+    size: u16,
+
+    #[prop(default = DEFAULT_FILL)]
+    fill: &'static str,
+
+    #[prop(default = DEFAULT_STROKE)]
+    stroke: &'static str,
+
+    #[prop(default = DEFAULT_STROKE_WIDTH)]
+    stroke_width: u16,
+
 ) -> impl IntoView {
     view! {
         <svg
           xmlns="http://www.w3.org/2000/svg"
+          class = class.to_string()
           width  =format!("{}", size)
           height =format!("{}", size)
           viewBox=format!("0 0 {} {}", size, size)
