e31c8951f8b79c3418518bdb3044409a12dfef78
Eugene Ryzhikov <eryzhikov@gmail.com>
2023-11-26 15:33:56 -0500

    Add routing to leptos project
    Added a routing functionality to the leptos project. This was achieved by extracting the content of the main application into a separate component that serves as the default route and providing a fallback route for non-existent paths.
    
    Switching to a routing architecture allows the application to grow in complexity by adding more routes/components without disrupting the existing codebase. The redirecting of non-existent paths to a "404 Page not found" component enhances user experience by giving clear feedback when navigating to a non-existent part of the application. The addition of leptos_router to the dependencies aids in managing the different views of the application.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-﻿use std::iter::Iterator;
-
-use leptos::ev::*;
-use leptos::logging::log;
-use leptos::*;
+﻿use leptos::*;
 use leptos_meta::*;
+use leptos_router::*;
 
-use components::*;
-use lepticons::*;
+use icons_view::*;
 
 mod components;
+mod icons_view;
 mod local_storage;
 
 //TODO show and select icon attributes
@@ -22,115 +19,22 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (icon_filter, set_icon_filter) = create_signal("".to_string());
-
-    let clear_filter = move |_| set_icon_filter.set("".to_string());
-
-    let on_input = move |ev: Event| {
-        set_icon_filter.set(event_target_value(&ev));
-        log!("Filter: {}", icon_filter.get_untracked());
-    };
-
     view! {
-        // <Meta name="color-scheme" content="light"/>
-        <div class="flex flex-row">
-            <div class="w-64 flex-none bg-secondary h-screen overflow-y-auto">
-
-                <StickyTop class="px-10 bg-gradient-to-b from-95% from-secondary to-100% to-transparent">
-                   //<div class="py-5 text-2xl font-medium">Lepticons</div>
-                   <img src="lepticons.png" class="py-5 w-48"/>
-                   <hr/>
-                </StickyTop>
-
-                <div class="px-10 pt-5 flex flex-col gap-2">
-                   {
-                       move || LucideGlyph::all_categories().iter()
-                             .filter(|(k, _)| !k.is_empty())
-                             .map(|(title, count)| {
-                                  let title_cloned = title.clone();
-                                  view! {
-                                    <div class="flex flex-row gap-4 text-sm text-primary/70 cursor-pointer">
-                                       <div class="flex-auto"
-                                               on:click=move |_| set_icon_filter.set(title_cloned.to_string()) >
-                                           {title}
-                                       </div>
-                                      <div class="flex-none text-primary/50 text-xs">{format!("{}", count)}</div>
-                                    </div>
-                                  }
-                              }).collect::<Vec<_>>()
-                   }
-                </div>
-
-            </div>
-            <div class="px-10 mt-5 flex flex-col gap-4 flex-auto h-screen overflow-y-auto">
-                <StickyTop class="bg-gradient-to-b from-85% from-background to-100% to-transparent">
-                    <MainMenu class="justify-end text-primary"/>
-                    <div class = "flex flex-row overflow-y-auto items-center w-full focus:border-orange-700/50 p-2 px-4 my-6 bg-secondary rounded-lg">
-                        <Icon glyph= move || LucideGlyph::Search/>
-                        <input type="text"
-                               class="flex-auto p-2 bg-transparent focus:outline-none  focus:border-1"
-                               prop:placeholder="Search icons..."
-                               prop:value={move || icon_filter.get()}
-                               on:input=on_input
-                        />
-                        <Icon glyph=move || LucideGlyph::X class="cursor-pointer" on:click=clear_filter />
-                    </div>
-                </StickyTop>
-                <IconTable icon_filter=icon_filter />
-
-            </div>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" view=IconsView/>
+                <Route path="/*any" view=NotFoundView/>
+            </Routes>
+        </Router>
     }
 }
 
 #[component]
-fn MainMenu(#[prop(default = "")] class: &'static str) -> impl IntoView {
+fn NotFoundView() -> impl IntoView {
     view! {
-        <div class={format!("flex flex-row gap-4 {}", class)}>
-            <a href="">Icons</a>
-            <a href="">Guide</a>
-            <a href="">Packages</a>
-            <a href="">License</a>
-            <ThemeToggle/>
-            <a href="https://github.com/eugener/lepticons"
-               target={"_blank".to_string()}
-               class="flex-none w-6 h-6  rounded-full bg-primary/100 text-secondary">
-                <Icon glyph= move || LucideGlyph::Github
-                      class="cursor-pointer p-[1px] pt-[3px] fill-secondary"/>
-            </a>
-        </div>
-    }
-}
-
-#[component]
-fn IconTable(icon_filter: ReadSignal<String>) -> impl IntoView {
-    let filtered_icons = move || LucideGlyph::find(icon_filter.get().to_lowercase().as_str());
-
-    view! {
-
-        <div class="flex flex-row flex-wrap gap-2 justify-between">
-        {
-            move || filtered_icons().iter().map( |icon|
-                view!{ <IconCell icon=icon.clone()/> }
-            ).collect::<Vec<_>>()
-        }
-        </div>
-
-    }
-}
-
-const ICON_CONTAINER:  &'static str = "relative group p-3.5 bg-secondary rounded-lg hover:bg-primary/20 border-1 border-primary/0 hover:border-primary/100 hover:border-1";
-const TOOLTIP:  &'static str = "absolute left-1/2 -translate-x-1/2 translate-y-5 z-10 opacity-0 transition-opacity group-hover:opacity-100 p-1 px-2 text-xs font-light text-white bg-orange-700/90 border border-1 border-orange-750/90 rounded";
-
-#[component]
-fn IconCell(icon: LucideGlyph) -> impl IntoView {
-    let glyph = icon.clone();
-    view! {
-        <div class=ICON_CONTAINER>
-            <Icon<LucideGlyph> glyph= move || glyph.clone() /> // stroke_width={1.7} stroke="#645e5f"/>
-            <div class=TOOLTIP >
-               {icon.name()}
-            </div>
+        <div class="flex flex-col items-center justify-center h-screen">
+            <div class="text-4xl font-medium">404</div>
+            <div class="text-2xl font-medium">Page not found</div>
         </div>
     }
 }
